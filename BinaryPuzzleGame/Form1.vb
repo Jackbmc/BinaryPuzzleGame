44af52941f5e371e8dd4af6f8991e79f859cd0f0
Jack <JackMc2208@gmail.com>
2024-09-18 11:51:56 +1000

    make grid array

--- BinaryPuzzleGame/Form1.vb
+++ BinaryPuzzleGame/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim grid(6, 6)
         'test
+    End Sub
+
+    Private Sub locatePairs()
+
     End Sub
 End Class
