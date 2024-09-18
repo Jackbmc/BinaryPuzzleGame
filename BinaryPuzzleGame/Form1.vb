dfb1ec7dc57efeaf416d0a34d096e63aaeb5bed1
Jack <JackMc2208@gmail.com>
2024-09-18 12:04:21 +1000

    started loading puzzle from file (currently just using old leaderboard code). to be finished

--- BinaryPuzzleGame/Form1.vb
+++ BinaryPuzzleGame/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Dim buttonArray(6, 6) As Button
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim grid(6, 6)
@@ -34,7 +36,28 @@
     End Sub
 
     Private Sub loadPuzzle()
-        'test
+        Dim filepath As String = "puzzle.txt"
+
+        If File.Exists(filepath) = False Then
+            File.Create(filepath).Dispose()
+        End If
+
+        If File.Exists(filepath) = True Then
+            ' Read the leaderboard data from the file
+            Dim leaderboard As List(Of String) = File.ReadAllLines(filepath).ToList()
+            Dim existingPlayerFound As Boolean = False
+            For i As Integer = 0 To leaderboard.Count - 1
+                Dim playerName As String = leaderboard(i).Substring(0, 3)
+                Dim playerScore As Integer = CInt(leaderboard(i).Substring(4))
+                If playerName = Name Then
+                    ' Update the score if the player already exists in the leaderboard
+                    playerScore = playerScore + 1
+                    leaderboard(i) = playerName & "," & CStr(playerScore)
+                    existingPlayerFound = True
+                    i = leaderboard.Count - 1
+                End If
+            Next i
+        End If
     End Sub
 
     Private Sub locatePairs()
